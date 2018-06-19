f98ca0f311b9db3ba4b583b338417b68abe7126a
fengzhizi715 <fengzhizi715@126.com>
2018-06-20 01:17:49 +0800

    init app2 module

---
+++ Retrofit/app2/src/main/java/com/kotlin/tutorial/retrofit/app2/activity/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.kotlin.tutorial.retrofit.app2.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ *
+ * @FileName:
+ *          com.kotlin.tutorial.retrofit.app2.activity.MainActivity.java
+ * @author: Tony Shen
+ * @date: 2018-06-20 01:14
+ * @version V1.0 <描述当前版本功能>
+ */
+class MainActivity : AppCompatActivity() {
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        initData()
+//    }
+//
+//    private fun initData() {
+//
+//        RetrofitManager.get()
+//                .apiService()
+//                .publicEvent("fengzhizi715")
+//                .compose(RxLifecycle.bind(this).toLifecycleTransformer())
+//                .compose(RxJavaUtils.maybeToMain())
+//                .subscribe()
+//    }
+}
