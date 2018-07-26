d5f543e1a85c097d63ab88ca78327adb745a0fcb
fengzhizi715 <fengzhizi715@126.com>
2018-07-26 22:49:10 +0800

    add Nothing.kt

---
+++ Basic/src/main/kotlin/Nothing.kt
@@ -0,0 +1,15 @@
+/**
+ * Created by tony on 2018/7/26.
+ */
+fun doForever(): Nothing {
+
+    while(true) {
+        println("do something...")
+    }
+}
+
+fun main(args: Array<String>) {
+
+    doForever()
+    println("done")
+}
