b4fc8ae7691893ded8cbc9a8e56f56930a1d2352
fengzhizi715 <fengzhizi715@126.com>
2018-07-26 17:32:27 +0800

    add Args2.kt

---
+++ Basic/src/main/kotlin/Args2.kt
@@ -0,0 +1,10 @@
+/**
+ * Created by tony on 2018/7/26.
+ */
+fun main(args: Array<String>) {
+
+    val array = arrayOf("java","kotlin","scala","grvoovy")
+
+    val list = toList(*array)
+    println(list)
+}
--- README.md
+++ README.md
@@ -12,7 +12,8 @@
 ## 基础篇
 Basic是Java工程，使用IntelliJ Idea打开。
 
-* Args.kt
+* Args1.kt
+* Args2.kt
 
 ## 进阶篇
 Basic是Java工程，使用IntelliJ Idea打开。
