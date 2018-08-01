3f81ee60b21c4217f3001722b8dd0b9b5472181f
fengzhizi715 <fengzhizi715@126.com>
2018-08-02 00:00:30 +0800

    add User.kt

--- Dagger2/app/src/main/java/com/kotlin/tutorial/dagger2/activity/MainActivity.kt
+++ Dagger2/app/src/main/java/com/kotlin/tutorial/dagger2/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kotlin.tutorial.dagger2
+package com.kotlin.tutorial.dagger2.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.kotlin.tutorial.dagger2.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ Dagger2/app/src/main/java/com/kotlin/tutorial/dagger2/domain/User.kt
@@ -0,0 +1,11 @@
+package com.kotlin.tutorial.dagger2.domain
+
+/**
+ *
+ * @FileName:
+ *          com.kotlin.tutorial.dagger2.domain.User.kt
+ * @author: Tony Shen
+ * @date: 2018-08-01 23:56
+ * @version V1.0 <描述当前版本功能>
+ */
+data class User(var name:String,var password:String)
