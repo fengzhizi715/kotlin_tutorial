e86300133e703388c8de351cc1cf57845f8fe50b
fengzhizi715 <fengzhizi715@126.com>
2018-08-21 00:22:29 +0800

    add Singleton.kt

---
+++ Basic/src/main/kotlin/classes/Singleton.kt
@@ -0,0 +1,14 @@
+package classes
+
+/**
+ * Created by tony on 2018/8/21.
+ */
+object Singleton {
+
+    fun printlnHelloWorld() = println("hello world")
+}
+
+fun main(args: Array<String>) {
+
+    Singleton.printlnHelloWorld()
+}
