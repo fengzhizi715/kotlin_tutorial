382dd7e1b55ca23cb655534e5a94b3df17c29ea0
fengzhizi715 <fengzhizi715@126.com>
2018-09-09 12:20:05 +0800

    add ChildCoroutine1.kt

---
+++ Coroutines1/src/main/kotlin/ChildCoroutine1.kt
@@ -0,0 +1,34 @@
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+
+/**
+ * Created by tony on 2018/9/9.
+ */
+fun main(args: Array<String>) {
+
+    // 创建一个协程，并创建两个子协程
+    val job = launch {
+
+        // 其一使用不同的上下文
+        val job1 = launch {
+
+            println("job1: I have my own context and execute independently!")
+            delay(1000)
+            println("job1: I am not affected by cancellation of the request")
+        }
+
+        // 另一个继承父级上下文
+        val job2 = launch(coroutineContext) {
+
+            println("job2: I am a child of the job coroutine")
+            delay(1000)
+            println("job2: I will not execute this line if my parent job is cancelled")
+        }
+    }
+
+    Thread.sleep(500)
+
+    job.cancel() // 取消job
+
+    Thread.sleep(2000)
+}
