02bd890ef2d2d82eb440533c0e5ea3abd88a83d8
fengzhizi715 <fengzhizi715@126.com>
2018-09-10 18:05:24 +0800

    update Select3.kt

--- Coroutines1/src/main/kotlin/Select3.kt
+++ Coroutines1/src/main/kotlin/Select3.kt
@@ -1,45 +1,49 @@
 import kotlinx.coroutines.experimental.cancelChildren
-import kotlinx.coroutines.experimental.channels.ReceiveChannel
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.consumeEach
+import kotlinx.coroutines.experimental.channels.SendChannel
 import kotlinx.coroutines.experimental.channels.produce
+import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.runBlocking
 import kotlinx.coroutines.experimental.selects.select
+import kotlin.coroutines.experimental.CoroutineContext
 import kotlin.coroutines.experimental.coroutineContext
 
 /**
  * Created by tony on 2018/9/10.
  */
-suspend fun selectProduces3(tony: ReceiveChannel<String>, monica: ReceiveChannel<String>): String = select<String> {
+fun produce(context: CoroutineContext, anotherChannel: SendChannel<
+        Int>) = produce<Int>(context) {
 
-    tony.onReceiveOrNull { value ->
-        if (value == null) "Channel 'tony' is closed"
-        else "This is $value"
-    }
+    for (num in 1..10) {
 
-    monica.onReceiveOrNull { value ->
-        if (value == null) "Channel 'monica' is closed"
-        else "This is $value"
+        delay(50)
+        select<Unit> {
+            onSend(num) {}                // 发送到当前 channel
+            anotherChannel.onSend(num) {} // 或发送到另一个 channel
+        }
     }
 }
 
 fun main(args: Array<String>) = runBlocking<Unit> {
+    val another = Channel<Int>() // 分配另一个 channel
 
-    val tony = produce<String>(coroutineContext) {
-        repeat(2) {
+    launch(coroutineContext) {
+
+        another.consumeEach {
+
+            println("Another channel consuming $it")
             delay(100)
-            send("Tony")
-        }
-    }
-    val monica = produce<String>(coroutineContext) {
-        repeat(2) {
-            delay(50)
-            send("Monica")
         }
     }
 
-    repeat(4) {
-        println(selectProduces2(tony, monica))
+    produce(coroutineContext, another).consumeEach {
+
+        println("Current channel consuming $it")
+        delay(200)
     }
 
-    coroutineContext.cancelChildren() // cancel tony & monica coroutines
+    println("Done!")
+    coroutineContext.cancelChildren()
 }
