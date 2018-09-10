043c23a487ca56ee6549e0414c81ddac897cb75c
fengzhizi715 <fengzhizi715@126.com>
2018-09-10 16:29:22 +0800

    add Actor.kt

---
+++ Coroutines1/src/main/kotlin/Actor.kt
@@ -0,0 +1,24 @@
+import kotlinx.coroutines.experimental.channels.actor
+import kotlinx.coroutines.experimental.runBlocking
+import kotlin.coroutines.experimental.coroutineContext
+
+/**
+ * Created by tony on 2018/9/10.
+ */
+fun main(args: Array<String>) = runBlocking<Unit> {
+
+    val summer = actor<Int>(coroutineContext) {
+        var sum = 0
+
+        for (i in channel) { // 不断接收channel中的数据，这个channel是ActorScope的变量
+            sum += i
+            println("Sum = $sum")
+        }
+    }
+
+    for (x in 1..10) {
+        summer.send(x)
+    }
+
+    summer.close()
+}
