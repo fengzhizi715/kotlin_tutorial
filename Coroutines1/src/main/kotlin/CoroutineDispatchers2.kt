a1ba096b203edf8b23d2a47898d396655dd7bf50
fengzhizi715 <fengzhizi715@126.com>
2018-09-10 14:42:08 +0800

    add CoroutineDispatchers2.kt

---
+++ Coroutines1/src/main/kotlin/CoroutineDispatchers2.kt
@@ -0,0 +1,32 @@
+import kotlinx.coroutines.experimental.*
+import kotlin.coroutines.experimental.coroutineContext
+
+/**
+ * Created by tony on 2018/9/10.
+ */
+fun main(args: Array<String>) = runBlocking(CommonPool) {
+
+    val jobs = ArrayList<Job>()
+
+    jobs += launch(Unconfined) { // 无限制
+        println("'Unconfined': I'm working in thread ${Thread.currentThread().name}")
+    }
+
+    jobs += launch(coroutineContext) { // 使用父级的上下文，也就是 runBlocking 的上下文
+        println("'coroutineContext': I'm working in thread ${Thread.currentThread().name}")
+    }
+
+    jobs += launch(CommonPool) {
+        println("'CommonPool': I'm working in thread ${Thread.currentThread().name}")
+    }
+
+    jobs += launch {
+        println("'default': I'm working in thread ${Thread.currentThread().name}")
+    }
+
+    jobs += launch(newSingleThreadContext("MyThread")) { // 创建自己的新线程
+        println("'MyThread': I'm working in thread ${Thread.currentThread().name}")
+    }
+
+    jobs.forEach { it.join() }
+}
