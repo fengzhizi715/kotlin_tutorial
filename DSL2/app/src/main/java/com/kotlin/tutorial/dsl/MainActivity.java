f6dc9e0b62d4a05f32419ea953df8a37d6ea3cb0
fengzhizi715 <fengzhizi715@126.com>
2018-09-18 17:27:43 +0800

    add anko's dependency

--- DSL2/app/src/main/java/com/kotlin/tutorial/dsl/MainActivity.java
+++ DSL2/app/src/main/java/com/kotlin/tutorial/dsl/MainActivity.java
@@ -3,8 +3,6 @@ package com.kotlin.tutorial.dsl;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import dsl.tutorial.kotlin.com.dsl2.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
