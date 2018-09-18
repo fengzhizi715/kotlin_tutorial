c207bea7c75bc23b9176d88f15ac6d7360b5956f
fengzhizi715 <fengzhizi715@126.com>
2018-09-18 17:09:30 +0800

    init dsl project

---
+++ DSL2/app/src/main/java/com/kotlin/tutorial/dsl/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kotlin.tutorial.dsl;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import dsl.tutorial.kotlin.com.dsl2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
