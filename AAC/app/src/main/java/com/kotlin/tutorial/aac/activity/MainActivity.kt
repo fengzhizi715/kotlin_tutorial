d445a5df83ea8cdd2b501b5f46c9d16a56fa7560
fengzhizi715 <fengzhizi715@126.com>
2018-10-08 15:48:37 +0800

    add AAC

---
+++ AAC/app/src/main/java/com/kotlin/tutorial/aac/activity/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.kotlin.tutorial.aac.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.kotlin.tutorial.aac.R
+
+/**
+ *
+ * @FileName:
+ *          com.kotlin.tutorial.aac.activity.MainActivity
+ * @author: Tony Shen
+ * @date: 2018-10-08 15:42
+ * @version: V1.0 <描述当前版本功能>
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
