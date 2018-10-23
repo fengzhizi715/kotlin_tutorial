769c975ad9818aef851e625b141349c62744f5a7
fengzhizi715 <fengzhizi715@126.com>
2018-10-23 15:09:05 +0800

    update LifeCycleListener.kt

--- AAC/app/src/main/java/com/kotlin/tutorial/aac/lifecycle/LifeCycleListener.kt
+++ AAC/app/src/main/java/com/kotlin/tutorial/aac/lifecycle/LifeCycleListener.kt
@@ -18,37 +18,37 @@ class LifeCycleListener : LifecycleObserver {
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun onCreate() {
-        Log.d(TAG, "onCreate: ")
+        Log.d(TAG, "onCreate")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onStart() {
-        Log.d(TAG, "onStart: ")
+        Log.d(TAG, "onStart")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
-        Log.d(TAG, "onResume: ")
+        Log.d(TAG, "onResume")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onPause() {
-        Log.d(TAG, "onPause: ")
+        Log.d(TAG, "onPause")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun onStop() {
-        Log.d(TAG, "onStop: ")
+        Log.d(TAG, "onStop")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun onDestroy() {
-        Log.d(TAG, "onDestroy: ")
+        Log.d(TAG, "onDestroy")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
     fun onAny() {
-        Log.d(TAG, "onAny: ")
+        Log.d(TAG, "onAny")
     }
 
     companion object {
