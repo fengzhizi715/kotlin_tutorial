cba4ee1c0bbd794b25307a24f0ef6228bef28bae
fengzhizi715 <fengzhizi715@126.com>
2018-11-30 18:37:46 +0800

    update Delay.kt

--- Coroutines1/src/main/kotlin/Delay.kt
+++ Coroutines1/src/main/kotlin/Delay.kt
@@ -6,20 +6,20 @@ import kotlinx.coroutines.*
 
 fun main(args: Array<String>) = runBlocking{
 
-    println("1: ${Thread.currentThread().name}")
+    println("1: current thread is ${Thread.currentThread().name}")
 
     GlobalScope.launch {
 
-        println("3: ${Thread.currentThread().name}")
+        println("3: current thread is ${Thread.currentThread().name}")
 
         delay(1000L)
 
-        println("4: ${Thread.currentThread().name}")
+        println("4: current thread is ${Thread.currentThread().name}")
     }
 
-    println("2: ${Thread.currentThread().name}")
+    println("2: current thread is ${Thread.currentThread().name}")
 
     Thread.sleep(2000L)
 
-    println("5: ${Thread.currentThread().name}")
+    println("5: current thread is ${Thread.currentThread().name}")
 }
