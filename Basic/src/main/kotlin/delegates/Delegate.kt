47599da5d915b776379fcb438c5272aba6296917
fengzhizi715 <fengzhizi715@126.com>
2018-12-04 17:10:50 +0800

    update readme.md

--- Basic/src/main/kotlin/delegates/Delegate.kt
+++ Basic/src/main/kotlin/delegates/Delegate.kt
@@ -38,13 +38,13 @@ class Student(studentId: Int, marks: Marks, totals: Totals)
 
 fun main(args: Array<String>) {
 
-    val student1 = Student(1, StdMarks(), StdTotals()) // Student 委托 StdMarks、StdTotals
+    val student1 = Student(1, StdMarks(), StdTotals()) // StdMarks、StdTotals 为 Student 被委托的类
     student1.printMarks()
     student1.printTotals()
 
     println("---------------------------")
 
-    val student2 = Student(2, ExcelMarks(), ExcelTotals()) // Student 委托 ExcelMarks、ExcelTotals
+    val student2 = Student(2, ExcelMarks(), ExcelTotals()) // ExcelMarks、ExcelTotals 为 Student 被委托的类
     student2.printMarks()
     student2.printTotals()
 }
--- README.md
+++ README.md
@@ -72,7 +72,7 @@ Basic 是Java工程，使用IntelliJ Idea打开。
 
 ### 委托、运算符重载以及中缀表达式
 
-* Delegate.kt
+* Delegate.kt （delegates包下）
 * Lazy.kt
 * Infix1.kt
 * Infix2.kt
