a1974e69b91deca0b1ff1e70e4d6ee7ef669f94f
fengzhizi715 <fengzhizi715@126.com>
2018-12-10 20:22:27 +0800

    增加注释

--- Benchmark/src/main/kotlin/benchmark/SequenceBenchmark.kt
+++ Benchmark/src/main/kotlin/benchmark/SequenceBenchmark.kt
@@ -8,12 +8,12 @@ import java.util.concurrent.TimeUnit
 /**
  * Created by tony on 2018-12-10.
  */
-@BenchmarkMode(Mode.Throughput)
-@Warmup(iterations = 3)
-@Measurement(iterations = 10, time = 5, timeUnit = TimeUnit.SECONDS)
-@Threads(8)
-@Fork(2)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode(Mode.Throughput) // 基准测试的模式，采用整体吞吐量的模式
+@Warmup(iterations = 3) // 预热次数
+@Measurement(iterations = 10, time = 5, timeUnit = TimeUnit.SECONDS) // 测试参数，iterations = 10 表示进行10轮测试
+@Threads(8) // 每个进程中的测试线程数
+@Fork(2)  // 进行 fork 的次数，表示 JMH 会 fork 出两个进程来进行测试
+@OutputTimeUnit(TimeUnit.MILLISECONDS) // 基准测试结果的时间类型
 open class SequenceBenchmark {
 
     @Benchmark
