c39b1cf8d2ffb75b6c7a17d412587c1095f85444
fengzhizi715 <fengzhizi715@126.com>
2018-12-17 11:21:40 +0800

    update DataClass

--- Basic/src/main/kotlin/classes/DataClass.kt
+++ Basic/src/main/kotlin/classes/DataClass.kt
@@ -12,6 +12,8 @@ fun main(args: Array<String>) {
     val user2 = user1.copy()
     println(user2)
 
+    println(user1===user2) // 判断 data class 的 copy 是否为深拷贝，如果二者内存地址指向不同则为深拷贝
+
     val user3 = user1.copy("monica")
     println(user3)
 
