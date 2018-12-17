100f61002da522e71aa9bb1d81ced2adbd61d271
fengzhizi715 <fengzhizi715@126.com>
2018-12-18 01:59:24 +0800

    update Demo4RetrofitActivity.kt

--- Coroutines2/app/src/main/java/com/kotlin/tutorial/coroutines/activity/Demo4RetrofitActivity.kt
+++ Coroutines2/app/src/main/java/com/kotlin/tutorial/coroutines/activity/Demo4RetrofitActivity.kt
@@ -9,10 +9,7 @@ import com.kotlin.tutorial.coroutines.model.PM25Model
 import com.kotlin.tutorial.coroutines.model.SO2Model
 import com.kotlin.tutorial.coroutines.model.ZipObject
 import kotlinx.android.synthetic.main.activity_demo_4_retrofit.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 /**
  *
@@ -37,28 +34,37 @@ class Demo4RetrofitActivity: AppCompatActivity() {
 
         GlobalScope.async(Dispatchers.Default) {
 
-            val pm25List = apiService.pm25(Constant.CITY_ID, Constant.TOKEN).await()
-
+            var pm25List:List<PM25Model>?=null
             var pm25Model:PM25Model? = null
 
-            pm25List.forEach {
+            try { // 由于每小时token请求数的限制，可能会导致接口不返回数据。如果不返回数据，则使用默认的PM25Model
+                pm25List = apiService.pm25(Constant.CITY_ID, Constant.TOKEN).await()
+                pm25List.forEach {
 
-                if ("南门" == it.position_name) {
+                    if ("南门" == it.position_name) {
 
-                    pm25Model = it
+                        pm25Model = it
+                    }
                 }
+            } catch (e: Exception) {
+                pm25Model = PM25Model()
             }
 
-            val so2List = apiService.so2(Constant.CITY_ID, Constant.TOKEN).await()
 
+            var so2List:List<SO2Model>? = null
             var so2Model:SO2Model? = null
 
-            so2List.forEach {
+            try { // 由于每小时token请求数的限制，可能会导致接口不返回数据。如果不返回数据，则使用默认的SO2Model
+                so2List = apiService.so2(Constant.CITY_ID, Constant.TOKEN).await()
+                so2List.forEach {
 
-                if ("南门" == it.position_name) {
+                    if ("南门" == it.position_name) {
 
-                    so2Model = it
+                        so2Model = it
+                    }
                 }
+            } catch (e: Exception) {
+                so2Model = SO2Model()
             }
 
             val zipObject = ZipObject(pm25Model?.quality,
@@ -67,15 +73,14 @@ class Demo4RetrofitActivity: AppCompatActivity() {
                     so2Model?.so2,
                     so2Model?.so2_24h)
 
-
             withContext(Dispatchers.Main) {
 
-                quality.setText("空气质量指数：" + zipObject.pm2_5_quality)
-                pm2_5.setText("PM2.5 1小时内平均：" + zipObject.pm2_5)
-                pm2_5_24h.setText("PM2.5 24小时滑动平均：" + zipObject.pm2_5_24h)
+                quality.setText("空气质量指数：" + zipObject.pm2_5_quality)        // 如果为空，表示使用了默认值
+                pm2_5.setText("PM2.5 1小时内平均：" + zipObject.pm2_5)             // 如果为0，表示使用了默认值
+                pm2_5_24h.setText("PM2.5 24小时滑动平均：" + zipObject.pm2_5_24h)  // 如果为0，表示使用了默认值
 
-                so2.setText("二氧化硫1小时平均：" + zipObject.so2)
-                so2_24h.setText("二氧化硫24小时滑动平均：" + zipObject.so2_24h)
+                so2.setText("二氧化硫1小时平均：" + zipObject.so2)  // 如果为0，表示使用了默认值
+                so2_24h.setText("二氧化硫24小时滑动平均：" + zipObject.so2_24h) // 如果为0，表示使用了默认值
             }
         }
     }
