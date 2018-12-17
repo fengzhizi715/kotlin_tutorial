e765a1f95a6663cb9e7718bf02ed8070b58a9de3
fengzhizi715 <fengzhizi715@126.com>
2018-12-17 20:56:28 +0800

    代码规范

--- Basic/src/main/kotlin/Lambda.kt
+++ Basic/src/main/kotlin/Lambda.kt
@@ -2,7 +2,6 @@
  * Created by tony on 2018/7/11.
  */
 fun main(args: Array<String>) {
-
     listOf(5, 12, 8, 33)
             .filter { it > 10 }
             .forEach { println(it) }
--- Basic/src/main/kotlin/MethodReference.kt
+++ Basic/src/main/kotlin/MethodReference.kt
@@ -5,7 +5,6 @@ import java.util.*
  * Created by tony on 2018/7/10.
  */
 fun main(args: Array<String>) {
-
     val u1 = User("tony")
     val u2 = User("cafei")
     val u3 = User("aaron")
