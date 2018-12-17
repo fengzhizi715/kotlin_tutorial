fdba915541b97b8b535ad0afeed6ff5e019e5a93
fengzhizi715 <fengzhizi715@126.com>
2018-12-17 12:52:43 +0800

    update DataClass

--- Basic/src/main/kotlin/classes/DataClass.kt
+++ Basic/src/main/kotlin/classes/DataClass.kt
@@ -3,16 +3,18 @@ package classes
 /**
  * Created by tony on 2018/8/15.
  */
-data class User(var name:String,var password:String)
+data class Address(var street:String)
+
+data class User(var name:String,var password:String,var address: Address)
 
 fun main(args: Array<String>) {
 
-    val user1 = User("tony","123456")
+    val user1 = User("tony","123456", Address("renming"))
 
     val user2 = user1.copy()
     println(user2)
 
-    println(user1===user2) // 判断 data class 的 copy 是否为深拷贝，如果二者内存地址指向不同则为深拷贝
+    println(user1.address===user2.address) // 判断 data class 的 copy 是否为浅拷贝，如果二者的address指向的内存地址相同则为浅拷贝，反之为深拷贝
 
     val user3 = user1.copy("monica")
     println(user3)
