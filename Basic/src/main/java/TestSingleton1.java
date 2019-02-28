a46f5b5a4b2b098e3b2691084a0de4345333a10d
fengzhizi715 <fengzhizi715@126.com>
2019-02-28 16:54:56 +0800

    add TestSingleton1

---
+++ Basic/src/main/java/TestSingleton1.java
@@ -0,0 +1,12 @@
+import classes.Singleton1;
+
+/**
+ * Created by tony on 2019-02-28.
+ */
+public class TestSingleton1 {
+
+    public static void main(String[] args) {
+
+        Singleton1.INSTANCE.printlnHelloWorld();
+    }
+}
