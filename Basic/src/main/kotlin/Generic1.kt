a821f4b63b8230aeb8d1b0d92aa124741abb6994
fengzhizi715 <fengzhizi715@126.com>
2019-06-17 19:18:21 +0800

    add Generic1.kt

---
+++ Basic/src/main/kotlin/Generic1.kt
@@ -0,0 +1,29 @@
+import java.lang.reflect.ParameterizedType
+
+/**
+ * Created by tony on 2019-06-17.
+ */
+
+object Generic1 {
+
+    open class InnerClass<T>
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val innerClass = object : InnerClass<Int>() {
+
+        } // 匿名内部类的声明在编译时进行，实例化在运行时进行
+
+        val typeClass = innerClass.javaClass.genericSuperclass
+        println(typeClass)
+
+        if (typeClass is ParameterizedType) {
+            val actualType = typeClass.actualTypeArguments[0]
+
+            println(actualType)
+        } else {
+            println("$typeClass is Not ParameterizedType")
+        }
+    }
+}
